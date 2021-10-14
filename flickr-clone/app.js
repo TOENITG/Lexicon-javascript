5108998f792345c07584a629400206a5cadbf68e
Tobias Englund <tobias.englund@fro.se>
2021-10-14 23:06:30 +0200

    last commit for today

--- flickr-clone/app.js
+++ flickr-clone/app.js
@@ -1,5 +1,5 @@
 const queryInput = document.querySelector("#query");
-// const searchForm = document.querySelector("#search-form");
+//const searchForm = document.querySelector("#search-form");
 const list = document.querySelector("#list");
 const thumbnails = document.querySelector("#thumbnails");
 const details = document.querySelector("#details");
@@ -9,8 +9,12 @@ const nextButton = document.querySelector("#next");
 const restartButton = document.querySelector("#restart");
 
 var currentPage = 1;
+var tags = "";
+var imageQuery = "recent";
 
-const getImages = async (imageQuery = "recent", tags = "") => {
+const getImages = async (action = "recent", tags = "") => {
+  console.log("action: ", action);
+  imageQuery = action;
   while (thumbnails.hasChildNodes()) {
     thumbnails.removeChild(thumbnails.firstChild);
   }
@@ -26,7 +30,7 @@ const getImages = async (imageQuery = "recent", tags = "") => {
     );
   }
   const data = await response.json();
-  console.log(data);
+  //  console.log(data);
   data.photos.photo.forEach((photo) => {
     let img = document.createElement("img");
     img.setAttribute(
@@ -42,7 +46,7 @@ const getImages = async (imageQuery = "recent", tags = "") => {
   });
 };
 
-getImages();
+getImages(imageQuery);
 
 const appendDetails = (key, value, element) => {
   let elementToAdd = document.createElement("p");
@@ -65,14 +69,14 @@ const imageDetails = async (server, id, secret) => {
     `https://www.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=244003ddf73f37254d5631da9fa91bed&photo_id=${id}&secret=${secret}&format=json&nojsoncallback=1`
   );
   const data = await response.json();
-  console.log(data.photo);
+  //  console.log(data.photo);
   appendDetails("Title: ", data.photo.title._content, details);
   appendDetails("User real name:", data.photo.owner.realname, details);
   appendDetails("Photo taken", data.photo.dates.taken, details);
 
   // Tags...
   let tags = data.photo.tags.tag;
-  console.log(tags);
+  //  console.log(tags);
   appendDetails("Tags: ", tags.length === 0 ? "none" : tags.length, details);
   let ul = document.createElement("ul");
   tags.forEach((tag) => {
@@ -87,20 +91,19 @@ const imageDetails = async (server, id, secret) => {
 nextButton.addEventListener("click", (e) => {
   e.preventDefault();
   currentPage++;
-  getImages();
+  getImages(imageQuery);
 });
 
 previousButton.addEventListener("click", (e) => {
   e.preventDefault();
   if (currentPage > 1) {
     currentPage--;
-    getImages();
+    getImages(imageQuery);
   }
 });
 
 hideMeButton.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("Here!");
   details.style.display = "none";
 });
 
@@ -108,11 +111,12 @@ restartButton.addEventListener("click", (e) => {
   e.preventDefault();
   details.style.display = "none";
   currentPage = 1;
-  getImages();
+  imageQuery = "recent";
+  getImages(imageQuery);
 });
 
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const query = queryInput.value;
-  getImages(query);
-});
+// searchForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const query = queryInput.value;
+//   getImages(query);
+// });
